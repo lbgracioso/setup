1b87ec3f5b5adc2b8b58e3b2fb2892f27caeba08
Lucas Gracioso <contact@lbgracioso.net>
2025-03-27 10:40:42 -0300

    Upload setup
    Signed-off-by: Lucas Gracioso <contact@lbgracioso.net>

---
+++ README.md
@@ -0,0 +1,56 @@
+# Linux Setup and Configuration
+This shellscript installs most of the software I use on my Linux for development, gaming or just normal use.
+
+## Installation
+1. Clone or download this repository to your local drive.
+2. Run `setup.sh` inside this directory.
+
+**Note:** Some applications may require user intervention during installation. Ensure you review the process as it runs.
+
+## Included applications, packages and drivers
+
+### Applications
+
+| Application                | Installation Method |
+|----------------------------|--------------------|
+| Steam                      | DNF                |
+| Polychromatic (Razer tools)| DNF (custom repo)  |
+| Discord                    | Flatpak            |
+| Extension Manager          | Flatpak            |
+| Spotify                    | Flatpak            |
+| DBeaver Community          | Flatpak            |
+| Gearlever                  | Flatpak            |
+| Telegram Desktop           | Flatpak            |
+| Lutris                     | Flatpak            |
+| Insomnia                   | Flatpak            |
+| Thunderbird                | Flatpak            |
+
+### Packages
+
+| Package                    | Installation Method |
+|----------------------------|--------------------|
+| vagrant                    | DNF                |
+| vagrant-libvirt            | Vagrant            |
+| podman                     | DNF                |
+| qemu-kvm                   | DNF                |
+| libvirt                    | DNF                |
+| ansible                    | DNF                |
+| gcc-c++                    | DNF                |
+| cmake                      | DNF                |
+| gdb                        | DNF                |
+| nodejs                     | DNF                |
+
+### Drivers
+
+| Driver                     | Installation Method |
+|----------------------------|--------------------|
+| epson-inkjet-printer-escpr | DNF                |
+
+## What to do next
+The script warns about the next to-dos at the end of installation.
+
+## Disclaimer
+This script was tailoredd to my personal setup and preferences. Use them at your own risk and review them before executing on your system.
+
+## License
+This repository is distributed under the terms of the GNU General Public License, version 3 or later. See the [LICENSE file](LICENSE) for details.
---
+++ setup.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+#
+# Author: Lucas Gracioso (contact@lbgracioso.net)
+#
+# This script automates the installation of some applications,
+# development tools and utilities I use.
+#
+# Note: Some applications may require user intervention, so ensure
+# you review the process as it runs.
+
+installGameLaunchers()
+{
+  echo "Installing game launchers..."
+  dnf install steam
+}
+
+installRazer()
+{
+  echo "Installing Razer tools..."
+  dnf config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
+  dnf install polychromatic
+}
+
+installFlatpakApps()
+{
+  echo "Installing general Flatpak apps..."
+
+  apps=(
+    "com.discordapp.Discord"
+    "com.mattjakeman.ExtensionManager"
+    "com.spotify.Client"
+    "io.dbeaver.DBeaverCommunity"
+    "it.mijorus.gearlever"
+    "org.telegram.desktop"
+    "net.lutris.Lutris"
+    "rest.insomnia.Insomnia"
+    "org.mozilla.Thunderbird"
+  )
+
+  for app in "${apps[@]}"; do
+    flatpak install -y flathub "$app"
+  done
+}
+
+installVagrantPlugins()
+{
+  echo "Installing Vagrant plugins..."
+  vagrant plugin install vagrant-libvirt
+}
+
+installWorkOps()
+{
+  echo "Installing work ops tools..."
+  dnf -y install podman qemu-kvm vagrant libvirt ansible
+}
+
+installWorkDev()
+{
+  echo "Installing work dev tools..."
+  dnf -y install gcc-c++ cmake gdb nodejs
+}
+
+installWork()
+{
+  echo "Installing work tools..."
+  installWorkOps
+  installWorkDev
+}
+
+installPrinterDrivers()
+{
+  echo "Installing printer drivers..."
+  dnf -y install epson-inkjet-printer-escpr
+}
+
+warnToDos()
+{
+  breakLine
+  echo "⚠️  What's left to do:"
+
+  todos=(
+    "Install JetBrains Toolbox"
+    "Install GNOME Extensions (Apps Menu, Dash to Dock, Places Status Indicator, Tray Icons: Reloaded, Desktop Icons NG (DING), Timer)"
+    "Configure SSH and GPG Keys (for GitHub)"
+  )
+
+  for i in "${!todos[@]}"; do
+    echo "$((i + 1)). ${todos[i]}"
+  done
+  breakLine
+}
+
+checkPrivileges()
+{
+  if [ "$EUID" -eq 0 ]; then
+    return
+  fi
+
+  echo "This script must be run as root."
+  read -r -p "Do you want to restart it with sudo? (y/N): " user_input
+
+  case "${user_input,,}" in
+    y|yes)
+      echo "Re-running the script with sudo..."
+      exec sudo "$0" "$@"
+      ;;
+    *)
+      echo "Exiting script..."
+      exit 1
+      ;;
+  esac
+}
+
+breakLine()
+{
+  echo -e "\n"
+}
+
+run()
+{
+  echo "###########################################"
+  echo "#            Starting setup...           #"
+  echo "###########################################"
+  breakLine
+
+  installFlatpakApps
+  installGameLaunchers
+  installRazer
+  installWork
+  warnToDos
+
+  breakLine
+  echo "###########################################"
+  echo "#             Setup done! Enjoy ;)        #"
+  echo "###########################################"
+}
+
+checkPrivileges
+run
